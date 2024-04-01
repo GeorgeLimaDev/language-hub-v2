64de834425ff00761d1ed5b91fd0e0f13e130898
georgelimadev <george.lima@vtex.com.br>
2024-04-01 09:08:38 -0300

    Setting up deploy configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://georgelimadev.github.io',
+	base: '/language-hub-v2',
 	integrations: [
 		starlight({
 			title: 'VTEX Language Hub',
